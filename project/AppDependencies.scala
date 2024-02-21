ecea84d5b65d56e075b65398f353c52d4c958add
Amar Jagtap <amar.jagtap@digital.hmrc.gov.uk>
2024-02-21 15:38:20 +0000

    TGP-213 copied over all the content of manage-your-trader-goods-profile-guide repo

---
+++ project/AppDependencies.scala
@@ -0,0 +1,15 @@
+import play.sbt.PlayImport._
+import sbt._
+
+object AppDependencies {
+  val bootStrapPlayVersion = "7.19.0"
+
+  val compile: Seq[ModuleID] = Seq(
+    ws,
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
+
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"   %% "bootstrap-test-play-28"     % bootStrapPlayVersion % "test",
+    "org.pegdown"   %  "pegdown"                    % "1.6.0"              % "test",
+    "org.scalatest" %% "scalatest"                  % "3.0.8"              % "test")
+}
